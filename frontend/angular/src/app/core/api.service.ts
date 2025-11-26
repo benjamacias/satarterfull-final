186f9182b0f7e4c67859f287c4df9a36769e1a92
benjamacias <74431154+benjamacias@users.noreply.github.com>
2025-11-26 16:59:45 +0100

    Configure Angular proxy for API

--- frontend/angular/src/app/core/api.service.ts
+++ frontend/angular/src/app/core/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = '/api';
 
 export interface Client {
   id: number;
